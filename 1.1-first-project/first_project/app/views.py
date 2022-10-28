d339606d52dd0930e3e47f3c44ba4e7cbf892292
Arthuras <a-r-ch@yandex.ru>
2022-10-28 14:17:54 +0300

    finished hw 1

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,3 +1,5 @@
+import datetime
+import os
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
@@ -7,13 +9,13 @@ def home_view(request):
     # впишите правильные адреса страниц, используя
     # функцию `reverse`
     pages = {
-        'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Обновить': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
+    # подбробнее о них мы поговорим на следующих лекциях 
     context = {
         'pages': pages
     }
@@ -21,15 +23,19 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
+    current_time = datetime.datetime.now().time()
+    msg = f"""   
+    <form action="/"">
+        <button>Назад</button>
+    </form><br>Текущее время: <b>{current_time}</b>"""
     return HttpResponse(msg)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+    listdir = os.listdir()
+    strdir = "<li>" + "<li>".join(listdir)
+    back = """
+            <form action="/" >
+                <button>Назад</button>
+            </form><br>"""
+    return HttpResponse(back + strdir)
