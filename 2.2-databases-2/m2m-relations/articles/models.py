ff066abe88bdaf14d8ed01194b46c99b437f2b66
Arthuras <a-r-ch@yandex.ru>
2022-11-19 14:22:09 +0300

    added hw2.2

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,16 +1,34 @@
 from django.db import models
 
 
-class Article(models.Model):
+class Tag(models.Model):
+    name = models.CharField(max_length=60, verbose_name='Имя тега')
 
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Тег'
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
-    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    published_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    scope_tag = models.ManyToManyField(Tag, through='Scope', related_name='scope_tag')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']        
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
--- 2.2-databases-2/orm_migrations/school/models.py
+++ 2.2-databases-2/orm_migrations/school/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Teacher(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
     subject = models.CharField(max_length=10, verbose_name='Предмет')
-
     class Meta:
         verbose_name = 'Учитель'
         verbose_name_plural = 'Учителя'
@@ -15,12 +14,16 @@ class Teacher(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     group = models.CharField(max_length=10, verbose_name='Класс')
-
+    teachers = models.ManyToManyField(Teacher, related_name='students')
     class Meta:
         verbose_name = 'Ученик'
         verbose_name_plural = 'Ученики'
 
     def __str__(self):
         return self.name
+
+
+# class TeacherStudent(models.Model):
+#     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='teachers')
+#     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='students')
